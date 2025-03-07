2c7d073b78e969448c9353da7792ec9b57487674
LCC-5-3090 <chenchen.liu@u.nus.edu>
2025-03-07 22:06:20 +0800

    try first half 2v2v, second half 4h4h, with half cat in hybrid

--- test_model.py
+++ test_model.py
@@ -5,18 +5,18 @@ from ultralytics.nn.modules.mamba_yolo import VSSBlock
 
 if __name__ == '__main__':
     
-    x = torch.randn(1, 4, 40, 40) # batch_size, channels, height, width
+    x = torch.randn(1, 128, 40, 40) # batch_size, channels, height, width
     x = x.cuda()
     print(x.shape)
     
-    model1 = A2C2f(c1=4, c2=64).cuda()
-    y = model1(x)
-    print(y.shape)
+    # model1 = A2C2f(c1=64, c2=64).cuda()
+    # y = model1(x)
+    # print(y.shape)
 
-    model2 = VSSBlock(in_channels=4, hidden_dim=128).cuda()
-    y = model2(x)
-    print(y.shape)
+    # model2 = VSSBlock(in_channels=64, hidden_dim=128).cuda()
+    # y = model2(x)
+    # print(y.shape)
 
-    model3 = HybridMambaAA(c1=4, c2=256).cuda()
+    model3 = HybridMambaAA(c1=128, c2=128).cuda()
     y = model3(x)
     print(y.shape)
