b62ed697b11763b3d0a3dbe294d559a2229161be
Chenchen Liu <chenchen.liu@u.nus.edu>
2025-03-12 15:59:34 +0800

    switch the core to RS-Mamba

--- ultralytics/nn/modules/common_utils_mbyolo.py
+++ ultralytics/nn/modules/common_utils_mbyolo.py
@@ -237,35 +237,57 @@ class CrossMergeOmni(torch.autograd.Function):
 # =============
 
 # cross selective scan ===============================
+# class SelectiveScanCore(torch.autograd.Function):
+#     # comment all checks if inside cross_selective_scan
+#     @staticmethod
+#     @torch.cuda.amp.custom_fwd
+#     def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1,
+#                 oflex=True):
+#         # all in float
+#         if u.stride(-1) != 1:
+#             u = u.contiguous()
+#         if delta.stride(-1) != 1:
+#             delta = delta.contiguous()
+#         if D is not None and D.stride(-1) != 1:
+#             D = D.contiguous()
+#         if B.stride(-1) != 1:
+#             B = B.contiguous()
+#         if C.stride(-1) != 1:
+#             C = C.contiguous()
+#         if B.dim() == 3:
+#             B = B.unsqueeze(dim=1)
+#             ctx.squeeze_B = True
+#         if C.dim() == 3:
+#             C = C.unsqueeze(dim=1)
+#             ctx.squeeze_C = True
+#         ctx.delta_softplus = delta_softplus
+#         ctx.backnrows = backnrows
+#         out, x, *rest = selective_scan_cuda_core.fwd(u, delta, A, B, C, D, delta_bias, delta_softplus, 1)
+#         ctx.save_for_backward(u, delta, A, B, C, D, delta_bias, x)
+#         return out
+
+#     @staticmethod
+#     @torch.cuda.amp.custom_bwd
+#     def backward(ctx, dout, *args):
+#         u, delta, A, B, C, D, delta_bias, x = ctx.saved_tensors
+#         if dout.stride(-1) != 1:
+#             dout = dout.contiguous()
+#         du, ddelta, dA, dB, dC, dD, ddelta_bias, *rest = selective_scan_cuda_core.bwd(
+#             u, delta, A, B, C, D, delta_bias, dout, x, ctx.delta_softplus, 1
+#         )
+#         return (du, ddelta, dA, dB, dC, dD, ddelta_bias, None, None, None, None)
+
+
 class SelectiveScanCore(torch.autograd.Function):
     # comment all checks if inside cross_selective_scan
     @staticmethod
     @torch.cuda.amp.custom_fwd
-    def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1,
-                oflex=True):
-        # all in float
-        if u.stride(-1) != 1:
-            u = u.contiguous()
-        if delta.stride(-1) != 1:
-            delta = delta.contiguous()
-        if D is not None and D.stride(-1) != 1:
-            D = D.contiguous()
-        if B.stride(-1) != 1:
-            B = B.contiguous()
-        if C.stride(-1) != 1:
-            C = C.contiguous()
-        if B.dim() == 3:
-            B = B.unsqueeze(dim=1)
-            ctx.squeeze_B = True
-        if C.dim() == 3:
-            C = C.unsqueeze(dim=1)
-            ctx.squeeze_C = True
+    def forward(ctx, u, delta, A, B, C, D=None, delta_bias=None, delta_softplus=False, nrows=1, backnrows=1, oflex=True):
         ctx.delta_softplus = delta_softplus
-        ctx.backnrows = backnrows
         out, x, *rest = selective_scan_cuda_core.fwd(u, delta, A, B, C, D, delta_bias, delta_softplus, 1)
         ctx.save_for_backward(u, delta, A, B, C, D, delta_bias, x)
         return out
-
+    
     @staticmethod
     @torch.cuda.amp.custom_bwd
     def backward(ctx, dout, *args):
