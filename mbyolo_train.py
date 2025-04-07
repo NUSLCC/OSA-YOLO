79013a8d7288b7c9f364b923b24b952f3393d444
CCL-Office <chenchen.liu@u.nus.edu>
2025-04-07 16:55:17 +0800

    add yolo12s

--- mbyolo_train.py
+++ mbyolo_train.py
@@ -4,10 +4,10 @@ import os
 import torch
 torch.use_deterministic_algorithms(True, warn_only=False)
 
-task_name = 'mambayolo_omni_2442_cbam_aug180'
+task_name = 'mambayolo_yolo12s_aug180'
 
 from clearml import Task
-task = Task.init(project_name="mamba-yolo-omni", task_name=task_name)
+task = Task.init(project_name="mamba-yolo-regular", task_name=task_name)
 
 current_path = os.path.abspath(os.getcwd())
 
@@ -15,7 +15,7 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--task', default='detect', help='train, val, test, speed or study')
     # Training settings
-    parser.add_argument('--model', type=str, default=current_path+'/ultralytics/cfg/models/mamba-yolo/Mamba-YOLO-T-Omni-CBAM.yaml', help='model path(s)')
+    parser.add_argument('--model', type=str, default=current_path+'/ultralytics/cfg/models/12/yolo12s.yaml', help='model path(s)')
     parser.add_argument('--data', type=str, default=current_path+'/ultralytics/cfg/datasets/VisDrone.yaml', help='dataset.yaml path')
     parser.add_argument('--epochs', type=int, default=300)
     parser.add_argument('--batch', type=int, default=16, help='batch size')
